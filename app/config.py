c24ab48bf4ad04fac9c1da19c6bb54c37cad3680
A6-z <az.ouhida@gmail.com>
2026-03-16 19:36:28 +0100

    no property enforcement for now

--- app/config.py
+++ app/config.py
@@ -1,5 +1,5 @@
 OBLIG_PROPERTIES = [
-    "patient_id", "record_id", "admission_id", "admission_date", "admission_type"
+    # "patient_id", "record_id", "admission_id", "admission_date", "admission_type"
 ]
 DT4H_LANGS = [
     'es', 'en', 'it', 'ro', 'cz', 'se', 'nl'
